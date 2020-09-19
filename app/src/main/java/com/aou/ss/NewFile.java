d05dfd06cd251c441f0d838f653a952c8778d0eb
nayelfares <nayel.fares@hotmail.com>
2020-09-19 20:34:41 +0400

    encoding+decoding finish

--- app/src/main/java/com/aou/ss/NewFile.java
+++ app/src/main/java/com/aou/ss/NewFile.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -22,7 +29,7 @@ import java.io.InputStream;
 
 public class NewFile extends AppCompatActivity {
     FileEncryptionManager mFileEncryptionManager;
-    String publicKey="d6f5g4sr68gs8r4g8s4rg";
+    String publicKey="zmZEKAEtOVb7hzlug8YuyMgmuQIZV39ogi2AntgqxW3qlDzQ9ivHAgViB79ZrJ6RTzb7VMc3KVHuJvtt1sGx5w==";
     String privateKey="s35fr4a6e8r4fa84rf86va4er";
     Integer PICKFILE_RESULT_CODE=1234;
     @Override
@@ -42,11 +49,14 @@ public class NewFile extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode==PICKFILE_RESULT_CODE && requestCode== Activity.RESULT_OK){
+        if (true){
             Uri uri = data.getData();
-            String src = uri.getPath();
+            String src = getPathFromUri(uri);
             try {
-                mFileEncryptionManager.setRSAKey(publicKey, privateKey, false);
+                mFileEncryptionManager.generateKey();
+                privateKey=mFileEncryptionManager.getPrivateKey();
+                publicKey=mFileEncryptionManager.getPublicKey();
+
                 byte[] data1 = FileUtils.getBytesFromInputStream(new FileInputStream(src));
 
                 File file=new File(Environment.getExternalStorageDirectory()+"/folderName");
@@ -61,11 +71,137 @@ public class NewFile extends AppCompatActivity {
                 File myappFile=new File(file
                         + File.separator + "myapp.bin");
 
+                File decFile=new File(file
+                        + File.separator + "1.jpg");
 
-                byte[] result = mFileEncryptionManager.encryptFileByPublicKey(data1,myappFile );
+                mFileEncryptionManager.encryptFileByPublicKey(data1,myappFile );
+                mFileEncryptionManager.decryptFileByPrivateKey(myappFile, decFile);
             }catch (Exception e){
                 Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+                Log.e("error",e.getMessage());
             }
         }
+    }
+    public  String getPathFromUri( final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(this,uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn( contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn( contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn( uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public  String getDataColumn( Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public  boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public  boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public  boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
     }
 }
