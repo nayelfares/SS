376fc1d9b9193fc24eecf3c0e55d72600f1fbcb2
Nayel <nayel.fares@hotmail.com>
2020-11-07 20:43:09 +0400

    get files list

--- app/src/main/java/com/aou/ss/auth/AuthManager.kt
+++ app/src/main/java/com/aou/ss/auth/AuthManager.kt
@@ -1,7 +1,18 @@
 package com.aou.ss.auth
 
+import android.content.Context
+import android.widget.Toast
+
 class AuthManager {
     companion object{
         var token=""
     }
+}
+
+fun String.toLink():String{
+    return "https://smartandsecureapplication.towarddevelopment.org/$this"
+}
+
+fun showToast(context: Context,message:String){
+    Toast.makeText(context,message,Toast.LENGTH_LONG).show()
 }
--- app/src/main/java/com/aou/ss/vm/FileAdapter.kt
+++ app/src/main/java/com/aou/ss/vm/FileAdapter.kt
@@ -1,15 +1,30 @@
 package com.aou.ss.vm
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.aou.ss.R
+import com.aou.ss.api.MainAPIManager
+import com.aou.ss.auth.FileUtils
+import com.aou.ss.auth.showToast
+import com.aou.ss.auth.toLink
 import com.aou.ss.data.ProjectFile
+import com.aou.ss.data.RequestInterface
 import com.blakequ.rsa.FileEncryptionManager
 import kotlinx.android.synthetic.main.file_item.view.*
+import okhttp3.ResponseBody
+import org.apache.commons.io.IOUtils
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.File
+import java.io.FileOutputStream
 
 
 class FileAdapter(val context:Context,val fileList:ArrayList<ProjectFile>) : RecyclerView.Adapter<FileAdapter.ViewHolder>() {
@@ -41,8 +56,8 @@ class FileAdapter(val context:Context,val fileList:ArrayList<ProjectFile>) : Rec
         }
 
         holder.fileIcon.setOnClickListener {
-            val mFileEncryptionManager = FileEncryptionManager.getInstance();
-            mFileEncryptionManager.setRSAKey(animal.public_key,animal.private_key,false)
+            download(animal.path.toLink(),position)
+
 
         }
     }
@@ -60,4 +75,42 @@ class FileAdapter(val context:Context,val fileList:ArrayList<ProjectFile>) : Rec
 
     }
 
+
+    fun download(link:String ,position: Int) {
+        val apiManager = MainAPIManager().provideRetrofitInterface().create(RequestInterface::class.java)
+        val loginVar = apiManager.downlload(link)
+        loginVar.enqueue(object : Callback<ResponseBody>{
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                showToast(context,"Downloadd")
+                val file = File(context.filesDir,"temp")
+                try {
+
+                    val fileOutputStream = FileOutputStream(file)
+                    IOUtils.write(response.body()!!.bytes(), fileOutputStream)
+                } catch (ex: Exception) {
+                }
+
+                val mFileEncryptionManager = FileEncryptionManager.getInstance();
+                mFileEncryptionManager.setRSAKey(fileList[position].public_key,fileList[position].private_key,true)
+                val decFile = File(context.filesDir, fileList[position].name)
+                mFileEncryptionManager!!.decryptFileByPrivateKey(file, decFile)
+                open(decFile.absolutePath)
+            }
+        })
+    }
+
+    fun open(filePath:String){
+        try {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setDataAndType(Uri.parse(filePath), FileUtils.getMimeType(filePath))
+            intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            context.startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            showToast(context,"no available app")
+        }
+    }
 }
